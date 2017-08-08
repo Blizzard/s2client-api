cd045997d49ea3cef9c287266927d3e66f54f2fb
BLIZZARD\abrunasso <abrunasso@blizzard.com>
2017-08-08 13:21:16 -0700

    Updated synchproject script to not combine libs.

--- synchproject.bat
+++ synchproject.bat
@@ -5,6 +5,8 @@ xcopy build\bin\libprotobuf.lib project\lib\ /s /e
 xcopy build\bin\libprotobufd.lib project\lib\ /s /e
 xcopy build\bin\sc2lib.lib project\lib\ /s /e
 xcopy build\bin\sc2libd.lib project\lib\ /s /e
+xcopy build\bin\sc2api.lib project\lib\ /s /e
+xcopy build\bin\sc2apid.lib project\lib\ /s /e
 xcopy build\bin\sc2protocol.lib project\lib\ /s /e
 xcopy build\bin\sc2protocold.lib project\lib\ /s /e
 xcopy build\bin\sc2renderer.lib project\lib\ /s /e
@@ -13,7 +15,7 @@ xcopy build\bin\sc2utils.lib project\lib\ /s /e
 xcopy build\bin\sc2utilsd.lib project\lib\ /s /e
 
 
-xcopy maps\* project\maps\ /s /e
+xcopy maps\* project\project\maps\ /s /e
 xcopy include\sc2api\* project\include\sc2api\ /s /e
 xcopy include\sc2lib\* project\include\sc2lib\ /s /e
 xcopy include\sc2renderer\* project\include\sc2renderer\ /s /e
