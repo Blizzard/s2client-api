8fbdf5930510f3a2a9a5a3543db3db8132dbc0bb
BLIZZARD\kcalderone <kcalderone@blizzard.com>
2017-09-15 16:50:07 -0700

    Exposing rendered interface actions.

--- include/sc2api/sc2_proto_to_pods.h
+++ include/sc2api/sc2_proto_to_pods.h
@@ -19,9 +19,11 @@ typedef MessageResponsePtr<SC2APIProtocol::ResponseQuery> ResponseQueryPtr;
 bool Convert(const ObservationPtr& observation_ptr, Score& score);
 bool Convert(const ObservationRawPtr& observation_ptr, Units& units);
 bool Convert(const ObservationPtr& observation_ptr, RenderedFrame& render);
-bool Convert(const ResponseObservationPtr& response_observation_ptr, RawActions& actions, const Units& units, uint32_t player_id);
-bool Convert(const ResponseObservationPtr& response_observation_ptr, SpatialActions& actions, const Units& units, uint32_t player_id);
 bool Convert(const ResponseGameInfoPtr& response_game_info_ptr, GameInfo& game_info);
+
+void ConvertRawActions(const ResponseObservationPtr& response_observation_ptr, RawActions& actions);
+void ConvertFeatureLayerActions(const ResponseObservationPtr& response_observation_ptr, SpatialActions& actions);
+void ConvertRenderedActions(const ResponseObservationPtr& response_observation_ptr, SpatialActions& actions);
 
 Race ConvertRaceFromProto(SC2APIProtocol::Race race);
 GameResult ConvertGameResultFromProto(SC2APIProtocol::Result result);
